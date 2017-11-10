809658236cb41c14b6f22ea53968a2a0b550e073
tdodwell <thomas.dodwell.13@ucl.ac.uk>
2017-11-10 13:00:05 +0000

    Added function to shorten code

--- muons.py
+++ muons.py
@@ -2,6 +2,16 @@
 
 from ROOT import TChain
 from ROOT import TLorentzVector
+
+def four_momentum_of_lepton(i_lepton, tree):
+    pt = tree.lep_pt[i_lepton]
+    eta = tree.lep_eta[i_lepton]
+    phi = tree.lep_phi[i_lepton]
+    e = tree.lep_E[i_lepton]
+    p = TLorentzVector()
+    p.SetPtEtaPhiE(pt,eta, phi, e)
+    return p
+
 data = TChain("mini")
 data.Add("http://opendata.atlas.cern/release/samples/Data/DataMuons.root")
 
@@ -17,7 +27,8 @@ for i_event in range(num_events_to_process):
         assert(n_leptons==2)
         pt1 = data.lep_pt[0]
         pt2 = data.lep_pt[1]
-        print("Leptons Pts are : "+str(pt1)+" , "+str(pt2))
-        p1 = TLorentzVector()
-        p1.SetPtEtaPhiE(pt1, data.lep_eta[0], data.lep_phi[0], data.lep_E[0])
+        print("Leptons Pts are : "+str(pt1)+" , "+str(pt2))    
+        p1 = four_momentum_of_lepton(0,data)
+        p2 = four_momentum_of_lepton(1,data)
         print("First lepton Pt from vector = "+str(p1.Pt()))
+        print("Second lepton Pt from vector = "+str(p2.Pt()))
