77d7f11189a78212ac8b191a3698b314416acbf3
tdodwell <thomas.dodwell.13@ucl.ac.uk>
2017-11-10 12:16:10 +0000

    added

--- muons.py
+++ muons.py
@@ -3,3 +3,13 @@
 from ROOT import TChain
 data = TChain("mini")
 data.Add("http://opendata.atlas.cern/release/samples/Data/DataMuons.root")
+
+num_events = data.GetEntries()
+print("Number of events = "+str(num_events))
+
+num_events_to_process = 10
+for i_event in range(num_events_to_process):
+    data.GetEntry(i_event)
+    n_leptons = data.lep_n
+    print("num of Leptons "+str(n_leptons))
+
