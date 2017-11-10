c161c24ba25906c9dfd560f8af19238246835a32
tdodwell <thomas.dodwell.13@ucl.ac.uk>
2017-11-10 15:11:36 +0000

    Function added to pair particles

--- muons.py
+++ muons.py
@@ -11,6 +11,28 @@ def four_momentum_of_lepton(i_lepton, tree):
     p.SetPtEtaPhiE(pt,eta, phi, e)
     return p
 
+def leptons_from_event(tree):
+    """
+    
+    """
+    leptons = []
+    n_leptons = tree.lep_n
+    for i_lepton in range(n_leptons):
+        p = four_momentum_of_lepton(i_lepton, tree)
+        leptons.append(p)
+    return leptons
+
+def pairs_from_particles(particles):
+    '''
+    '''
+    pairs = []
+    n_particles = len(particles)
+    for i in range(n_particles):
+        for j in range(i+1 , n_particles):
+            pair = (particles[i], particles[j])
+            pairs.append(pair)
+    return pairs
+
 data = TChain("mini")
 data.Add("http://opendata.atlas.cern/release/samples/Data/DataMuons.root")
 
@@ -24,17 +46,20 @@ for i_event in range(num_events_to_process):
     data.GetEntry(i_event)
     n_leptons = data.lep_n
     if n_leptons >= 2:
-        print("num of Leptons "+str(n_leptons))
-        assert(n_leptons==2)
-        pt1 = data.lep_pt[0]
-        pt2 = data.lep_pt[1]
-        print("Leptons Pts are : "+str(pt1)+" , "+str(pt2))    
-        p1 = four_momentum_of_lepton(0,data)
-        p2 = four_momentum_of_lepton(1,data)
-        print("Lepton Pt from vector = "+str(p1.Pt())+" , " +str(p2.Pt()) )
+        #print("num of Leptons "+str(n_leptons))
+        leptons = leptons_from_event(data)
+        pairs = pairs_from_particles(leptons) 
+
+        # TODO: treat casse of more than two leptons
+
+ #       print("Leptons Pts are : "+str(pt1)+" , "+str(pt2))
+        pair = pairs[0]
+        p1 = pair[0]
+        p2 = pair[1]
+#        print("Lepton Pt from vector = "+str(p1.Pt())+" , " +str(p2.Pt()) )
         ppair = p1 + p2
         mpair = ppair.M()
-        print("Invariant Mass of lepton pair = " +str(mpair))
+#        print("Invariant Mass of lepton pair = " +str(mpair))
         h_mpair.Fill(mpair/1000)    #converts MeV to GeV
 
 h_mpair.Draw()
